39569db1814f76e8fb64a74e4e831d85b07ac9be
emil-nabil333 <elbrinselomda@yahoo.com>
2022-09-10 07:51:01 -0700

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,65 @@
+#!/bin/sh # 
+ # # Command: wget https://raw.githubusercontent.com/emilnabil/picon-nilesat-khaled/main/installer.sh -qO - | /bin/sh # # ########################################### ###########################################  
+MY_URL="https://raw.githubusercontent.com/emilnabil/picon-nilesat-khaled/main"  
+echo "******************************************************************************************************************"
+echo "    download and install picon  "
+echo "============================================================================================================================="
+#####################################################################################
+echo "         install picon-nilesat    "
+cd /tmp
+set -e 
+wget -q  "https://raw.githubusercontent.com/emilnabil/picon-nilesat-khaled/main/picon1.tar.gz"
+wait
+tar -xzf picon1.tar.gz  -C /
+wait
+cd ..
+set +e
+rm -f /tmp/picon1.tar.gz
+echo "==========================================================================================================================="
+echo "         install picon-nilesat    "
+cd /tmp
+set -e 
+wget -q  "https://raw.githubusercontent.com/emilnabil/picon-nilesat-khaled/main/picon2.tar.gz"
+wait
+tar -xzf picon2.tar.gz  -C /
+wait
+cd ..
+set +e
+rm -f /tmp/picon2.tar.gz
+sleep 2;
+echo "" 
+echo "" 
+echo "****************************************************************************************************************************"
+echo "# PICON  INSTALLED SUCCESSFULLY #"
+echo "
+echo " "*********************************************************" 
+	echo "********************************************************************************"
+echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
+sleep 4;
+	echo '========================================================================================================================='
+###########################################                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+echo "**********************************************************************************"
+wait
+killall -9 enigma2
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
