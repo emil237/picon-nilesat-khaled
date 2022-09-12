5e7b858c744277195b88b41566905b7ef501570c
emil-nabil333 <elbrinselomda@yahoo.com>
2022-09-12 11:11:24 -0700

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -8,7 +8,7 @@ echo "==========================================================================
 echo "         install picon-nilesat    "
 cd /tmp
 set -e 
-wget -q  "https://raw.githubusercontent.com/emilnabil/picon-nilesat-khaled/main/picon1.tar.gz"
+wget -q  "https://github.com/emilnabil/picon-nilesat-khaled/raw/main/picon1.tar.gz"
 wait
 tar -xzf picon1.tar.gz  -C /
 wait
@@ -19,7 +19,7 @@ echo "==========================================================================
 echo "         install picon-nilesat    "
 cd /tmp
 set -e 
-wget -q  "https://raw.githubusercontent.com/emilnabil/picon-nilesat-khaled/main/picon2.tar.gz"
+wget -q  "https://github.com/emilnabil/picon-nilesat-khaled/raw/main/picon2.tar.gz"
 wait
 tar -xzf picon2.tar.gz  -C /
 wait
@@ -43,6 +43,7 @@ echo "**************************************************************************
 wait
 killall -9 enigma2
 exit 0
+
 
 
 
